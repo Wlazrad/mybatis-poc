a3ec5fc27bb67b9e8bfdc806996ea7eb67676393
Radosław Wlazły <radek.wlazly@gmail.com>
2023-08-07 15:41:32 +0200

    init

---
+++ src/main/java/com/example/mybatispoc/MybatisPocApplication.java
@@ -0,0 +1,13 @@
+package com.example.mybatispoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mybatispoc/MybatisPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mybatispoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
